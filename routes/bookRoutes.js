072a6bc582eb01adc303bf96934c784a0f36783f
Saurabh Ganvir <saurabhganvir14@gmail.com>
2025-06-03 18:32:20 +0530

    Adding auth routes and book routes

---
+++ routes/bookRoutes.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import {createBook, getBooks, getBookById} from '../controllers/bookController.js'
+import authenticationToken from '../middlewares/auth.js';
+import {createReview} from '../controllers/reviewController.js';
+
+const router= express.Router();
+
+router.route('/').post(authenticationToken, createBook).get(getBooks);
+
+router.get('/:id',getBookById);
+router.post('/:id/reviews', authenticationToken, createReview);
+
+module.exports= router;
