45e429896174ad3362a30218fd38f4cac35c23cb
Saurabh Ganvir <saurabhganvir14@gmail.com>
2025-06-03 13:15:45 +0530

    Adding revire controllers like createReview, updateReview and DeleteReview

---
+++ controllers/reviewController.js
@@ -0,0 +1,116 @@
+import Review from '../models/reviewModel.js';
+import Book from '../models/bookModel.js';
+
+//create a review for a book
+//post/books/:id/reviews
+
+const createReview = async (req, res, next)=>{
+    try {
+        const bookId  = req.params.id;
+
+        //check if book exists
+        const book = await Book.findById(bookId);
+        if(!book){
+            return res.status(404).json({
+                success:false,
+                message:'Book not found'
+            });
+        }
+
+        //check if user already reviewed
+        const existingReview = await Review.findOne({
+            book:bookId,
+            user: req.user.id
+        });
+
+        if(existingReview){
+            return res.status(409).json({
+                success:false,
+                message:'You have already reviewed this book'
+            });
+        }
+        
+        const review = await Review.create({
+            ...req.body,
+            book:bookId,
+            user: req.user.id
+        });
+
+        await review.populate('user', 'username name');
+
+        res.status(201).json({
+            success:true,
+            message: 'Review created successfully',
+            data: review
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+//updating review
+//put/reviews/:id
+
+const updateReview = async(req, res, next)=>{
+    try {
+        const review = await Review.findOne({
+            _id: req.params.id,
+            user: req.user.id
+        });
+
+        if(!review){
+            return res.status(404).json({
+                success:false,
+                message:"Review not found or not authorised",
+            });
+        }
+
+        const updatedReview = await Review.findByIdAndUpdate(
+            req.params.id, req.body, {new:true, runValidators: true}
+        ).populate('user', 'username name');
+
+        res.json({
+            success:true,
+            message:" Review updated successfully",
+            data: updatedReview
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+// deleting a review
+//delete/reviews/:id
+
+const deleteReview = async(req, res, next)=>{
+    try {
+        const review = await Review.findOne({
+            _id:req.params.id,
+            user:req.user.id
+        });
+
+        if(!review){
+            return res.status(404).json({
+                success:false,
+                message:'Review not found or not authorised'
+            });
+        }
+
+        await review.findByIdAndDelete(req.params.id);
+
+        res.json({
+            success: true,
+            message: "Review deleted successfully"
+        });
+
+    } catch (error) {
+        next(error);
+    }
+};
+
+module.exports= {
+    createReview, updateReview, deleteReview
+}
